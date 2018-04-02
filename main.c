50c6f8f4517d934250574c10a1a41f1a679964ec
qibaowei-guet <34730733+qibaowei-guet@users.noreply.github.com>
2018-04-02 15:39:11 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,35 @@
 
 int main(int argc , char **argv)
 {
-    YUV_E eYUV = YUV420 ;
-    bool ret = RGB24_SaveYUVFile(eYUV , argv[1]);
-    return 0;
+    bool ret = -1;
+    YUV_E eYUV  ;
+
+    if ( argc != 3 ){
+        printf("\nUsage:%s <xxx.bmp> <index>\n",argv[0]);
+        printf("index:\n");
+        printf("\t 0) RGB24 To YUV444.\n");
+        printf("\t 1) RGB24 To YUV422.\n");
+        printf("\t 2) RGB24 To YUV420.\n");
+        printf("Example: %s xxx.bmp 2 \n",argv[0]);
+        return ret ;
+    }
+
+    switch(atoi(argv[2]))
+    {
+    case 0 :
+        eYUV = YUV444 ;
+        break;
+    case 1 :
+        eYUV = YUV422 ;
+        break;
+    case 2 :
+        eYUV = YUV420 ;
+        break;
+    default:
+        eYUV = YUV420 ;
+        break;
+    }
+
+    ret = RGB24_SaveYUVFile(eYUV , argv[1]);
+    return ret;
 }
